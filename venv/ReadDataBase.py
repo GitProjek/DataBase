7a82e48c7a8bc5da576208e62b984e9611d8841c
Git <gitbooon@gmail.com>
2022-04-22 01:32:48 +0300

    ReadDataBase-2

--- venv/ReadDataBase.py
+++ venv/ReadDataBase.py
@@ -7,3 +7,7 @@ with open('products.txt', encoding='UTF-8') as data:
         product = data.readline()
         if not product:
             break
+        prd = Product()
+        prd.title, prd.cost, prd.quantity, prd.unit_of_measure = product.split()
+        data_base.append(prd)
+        print(prd.title, prd.cost, prd.quantity, prd.unit_of_measure)
